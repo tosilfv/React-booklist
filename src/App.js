b1aa97de71c22a83845148d8d154ee6f95354ed6
Toni Silfver <toni.silfver@protonmail.ch>
2022-11-02 12:07:51 +0200

    v3 add NavBar, BookContext, useStorageTabReducer, TabReducer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./styles/theme";
 import BookRouter from "./routes/BookRouter";
+import BookProvider from "./contexts/BookContext";
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <BookRouter />
-    </ThemeProvider>
+    <BookProvider>
+      <ThemeProvider theme={theme}>
+        <BookRouter />
+      </ThemeProvider>
+    </BookProvider>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { Box, Tabs } from "@mui/material";
+import { TabContext } from "../contexts/BookContext";
+import { HOME_TAB } from "../constants/bookConstants";
+import LinkTab from "../components/LinkTab";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  const [tab, setTab] = useContext(TabContext);
+  const handleChange = (evt, newTab) => {
+    setTab({ type: "NEW-TAB", tab: newTab });
+  };
+  // starting the app sets navbar tab to HOME
+  useEffect(() => {
+    setTab({ type: "NEW-TAB", tab: HOME_TAB });
+  }, [setTab]);
+  return (
+    <Box>
+      <Tabs
+        variant="fullWidth"
+        value={tab}
+        onChange={handleChange}
+        aria-label="nav tabs"
+      >
+        <LinkTab label="Home" pathname="/" />
+        <LinkTab label="Add Book" pathname="/bookform" />
+      </Tabs>
+    </Box>
+  );
 }
